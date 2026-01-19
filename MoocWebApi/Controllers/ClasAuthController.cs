96dc7a2a06cb0eda9dc370567d77def21d355632
buyi0003 <166275426+Buyincode@users.noreply.github.com>
2026-01-19 11:15:04 +1030

    get data by id and got succssed.

--- MoocWebApi/Controllers/ClasAuthController.cs
+++ MoocWebApi/Controllers/ClasAuthController.cs
@@ -1,3 +1,6 @@
+using Mooc.Application.Contracts.Demo;
+using Mooc.Application.Demo;
+
 namespace MoocWebApi.Controllers;
 
 [ApiController]
@@ -7,28 +10,38 @@ public class ClasAuthController : ControllerBase
 {
 
     private static readonly List<RegistrationDto> users = [
-new(
-"yes",
-"123324@.com",
-1234567789,
-"male",
-12,
-"qwertyuiop123!@#"
-), new(
 
-"agre",
-"54321@.com",
-65434321,
-"male",
-17,
-"oiuytrew123!@#"
+ new RegistrationDto(
+        Id: 1,
+        UserNmae: "yes",
+        Email: "123324@.com",
+        PhoneNumber: 1234567789,
+        Gender: "male",
+        Age: 12,
+        PassWord: "qwertyuiop123!@#"
+    ),
+    new RegistrationDto(
+        Id: 2,
+        UserNmae: "agre",
+        Email: "54321@.com",
+        PhoneNumber: 65434321,
+        Gender: "male",
+        Age: 17,
+        PassWord: "oiuytrew123!@#"
 )
 ];
 
-  [HttpGet]
-    public IActionResult GetAllUsers()
+   
+    [HttpGet("{id:int}")]
+     public IActionResult GetById(int id) 
     {
-        return Ok(users);
+        var user = users.FirstOrDefault(u => u.Id == id);
+
+        if (user == null)
+            return NotFound("User not found");
+
+        return Ok(user);
     }
+    
 
 }
