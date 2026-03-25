6b0bf4cd949b65b549051481a7610d1619c39097
Edward Chen <kunhong03@gmail.com>
2026-03-26 08:35:09 +1100

    fix: use seperate User entity file

---
+++ Mooc.Model/Entity/Gender.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mooc.Model.Entity
+{
+    /// <summary>
+    /// Gender Enum
+    /// </summary>
+    public enum Gender
+    {
+        /// <summary>
+        /// Male gender
+        /// </summary>
+        Male = 1,
+
+        /// <summary>
+        /// Female gender
+        /// </summary>
+        Female = 2,
+
+        /// <summary>
+        /// Other gender
+        /// </summary>
+        Other = 3
+    }
+}
